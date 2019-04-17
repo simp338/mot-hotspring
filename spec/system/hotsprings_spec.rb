79212c2eafbdc2de505b688ba7516c3a779eddd7
simp338 <45528081+simp338@users.noreply.github.com>
2019-04-17 13:33:32 +0000

    RSpec DRY by using 'let'

--- spec/system/hotsprings_spec.rb
+++ spec/system/hotsprings_spec.rb
@@ -2,27 +2,40 @@ require 'rails_helper'
 
 describe '施設管理機能', type: :system do
   describe '一覧表示機能' do
+      #ユーザーAを定義
+      let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@a.com') }
+      #ユーザーBを定義
+      let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@b.com') }
+      
     before do
-      # ユーザーAを作成しておく
-      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@a.com')
-      # 登録者がユーザーAである施設を作成しておく
+      # 施設を作成しておく
       hotspring_a = FactoryBot.create(:hotspring)
-      # 両者を紐付けるアソシエーションを作成する
+      # ユーザーAと紐付けるアソシエーションを作成する（かつuser_aを呼ぶことでユーザーAを作成している）
       FactoryBot.create(:relationship, user: user_a, hotspring: hotspring_a)
+      
+        visit login_path
+        fill_in 'メールアドレス', with: login_user.email
+        fill_in 'パスワード', with: login_user.password
+        click_button 'ログイン'
     end
     
     context 'ユーザーAがログインしているとき' do
-      before do
-        # ユーザーAでログインする
-        visit login_path
-        fill_in 'メールアドレス', with: 'a@a.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログイン'
-      end
+      #login_user = user_aで定義
+      let(:login_user) { user_a }
       
       it 'ユーザーAが登録した施設が表示される' do
         # 作成済みの施設名称が画面上に表示されていることを確認'
         expect(page).to have_content 'テスト'
+      end
+    end
+    
+    context 'ユーザーBがログインしているとき' do
+      #login_user = user_bで定義
+      let(:login_user) { user_b }
+      
+      it 'ユーザーAが登録した施設が表示されない' do
+        #ユーザーAが登録した施設名称が画面上に表示されないことを確認
+        expect(page).to have_no_content 'テスト'
       end
     end
   end
