9698f0e9c46731a70dbad5db0e19d66157007fff
simp338 <45528081+simp338@users.noreply.github.com>
2019-04-18 12:50:35 +0000

    Add test & RSpec DRY using by 'shared_examples_for'

--- spec/factories/hotsprings.rb
+++ spec/factories/hotsprings.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :hotspring do
-    name { 'テスト' }
+    name { 'カーブドッチ　ヴィネスパ' }
     url { 'https://hotel.travel.rakuten.co.jp/hinfo/?f_no=162980' }
     code { '162980' }
     image_url { 'https://trvimg.r10s.jp/share/image_up/162980/LARGE/3f91656ffe4dfae7bbd7aa54467957592d329556.47.1.26.2.jpg' }
--- spec/system/hotsprings_spec.rb
+++ spec/system/hotsprings_spec.rb
@@ -1,42 +1,47 @@
 require 'rails_helper'
 
 describe '施設管理機能', type: :system do
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@a.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@b.com') }
+  let(:hotspring_a) { FactoryBot.create(:hotspring) }
+  let!(:relationship_a) { FactoryBot.create(:relationship, user: user_a, hotspring:hotspring_a) }
+  
+  shared_examples_for 'ユーザーAが登録した施設が表示される' do
+    it { expect(page).to have_content 'カーブドッチ　ヴィネスパ' }
+  end
+      
+  before do
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログイン'
+  end  
+  
   describe '一覧表示機能' do
-      #ユーザーAを定義
-      let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@a.com') }
-      #ユーザーBを定義
-      let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@b.com') }
-      
-    before do
-      # 施設を作成しておく
-      hotspring_a = FactoryBot.create(:hotspring)
-      # ユーザーAと紐付けるアソシエーションを作成する（かつuser_aを呼ぶことでユーザーAを作成している）
-      FactoryBot.create(:relationship, user: user_a, hotspring: hotspring_a)
-      
-        visit login_path
-        fill_in 'メールアドレス', with: login_user.email
-        fill_in 'パスワード', with: login_user.password
-        click_button 'ログイン'
-    end
-    
     context 'ユーザーAがログインしているとき' do
-      #login_user = user_aで定義
       let(:login_user) { user_a }
-      
-      it 'ユーザーAが登録した施設が表示される' do
-        # 作成済みの施設名称が画面上に表示されていることを確認'
-        expect(page).to have_content 'テスト'
-      end
+    
+      it_behaves_like 'ユーザーAが登録した施設が表示される'
     end
     
     context 'ユーザーBがログインしているとき' do
-      #login_user = user_bで定義
       let(:login_user) { user_b }
       
       it 'ユーザーAが登録した施設が表示されない' do
-        #ユーザーAが登録した施設名称が画面上に表示されないことを確認
         expect(page).to have_no_content 'テスト'
       end
+    end
+  end
+  
+  describe '詳細表示機能' do
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) { user_a }
+      
+      before do
+        visit "/hotspring/#{hotspring_a.code}"
+      end
+      
+      it_behaves_like 'ユーザーAが登録した施設が表示される'
     end
   end
 end
