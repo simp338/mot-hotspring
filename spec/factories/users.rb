8cc1f7fe2f739ccefc3cd72dbedb3c4033eaa214
simp338 <45528081+simp338@users.noreply.github.com>
2019-04-20 12:50:48 +0000

    RSpec add user-create-test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name { 'テストユーザー' }
-    email { 'test1@example.com' }
+    sequence(:email) {|n| "test#{n}@t#{n}.com" }
     password { 'password' }
   end
 end
--- spec/system/hotsprings_spec.rb
+++ spec/system/hotsprings_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
+describe 'ユーザー管理機能', type: :system do
+  describe '新規ユーザー登録機能' do
+    let(:user_success) { FactoryBot.build(:user) }
+    let(:user_none) { FactoryBot.build(:user, name: '', email: '', password: '') }
+
+    before do
+      visit signup_path
+      fill_in 'お名前', with: new_user.name
+      fill_in 'メールアドレス', with: new_user.email
+      fill_in 'パスワード', with: new_user.password
+      fill_in 'パスワード（確認）', with: new_user.password
+      click_button '登録する' 
+    end
+    
+    context '新規登録画面で全て入力したとき' do
+      let(:new_user) { user_success }
+      
+      it '正常に登録される' do
+        expect(page).to have_selector '.alert-success', text: 'ユーザを登録しました。'
+      end
+    end
+    
+    context '新規登録画面で入力しなかったとき' do
+      let(:new_user) { user_none }
+      
+      it '登録に失敗する' do
+        expect(page).to have_selector '.alert-danger', text: 'ユーザの登録に失敗しました。'
+      end
+    end
+  end
+end
+  
+
 describe '施設管理機能', type: :system do
   let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@a.com') }
   let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@b.com') }
